5d44a51c95dca0ec11b5a213caabddd28f51d6fa
hoangmartin <hoang@lacroixrouge-brest.fr>
2017-10-10 10:16:57 +0200

    Add All Files

--- client_udp_test.py
+++ client_udp_test.py
@@ -2,10 +2,51 @@
 # coding: utf-8
 
 import socket
+from random import randint
 import time
 
-client='192.168.0.231'
-PORT=6000
-#ligne creation trame
-sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendto("Hello",(client,PORT))
+#client,PORT='192.168.0.231',6000
+client,PORT="127.0.0.1",12000
+ID=0
+
+#générer des valeurs
+while True:
+    ID+=1
+    Taille=randint(0,100)
+    GV=randint(0,100)
+    SF=randint(0,100)
+#creation d'un trmae
+    trame=bytearray([ID,Taille,GV,SF])
+    sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(trame,(client,PORT))
+    print "Message envoyé:",trame[0],trame[1],trame[2],trame[3]
+    
+##    
+#afficher des infos sur l'écran
+
+    print "===============================\n"
+    print "Réponse du serveur"
+    response, addr=sock.recvfrom(100)
+    print "Trame de réponse:", response.encode("hex")
+    print "Latitude:",(response[4])
+    print "Longitude:",(response[5])
+    print "ID",ord(response[0])
+    print "Vitesse du vent: ",ord(response[1])
+    print "Direction du vent:",ord(response[2])
+    print "Gite:",ord(response[3])
+    time.sleep(2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- serveur_udp.py
+++ serveur_udp.py
@@ -2,14 +2,46 @@
 # coding: utf-8
 
 import socket
- 
-server="127.0.0.1"
-PORT=5000
+from random import randint
 
-sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-sock.bind((server,PORT))
+#server,PORT='192.168.0.231',6000
+server,port="127.0.0.1",12000
 
+print "Waiting connection..."
 while True:
-    data,addr=sock.recvfrom(5) #ligne decodage de la trame
-    print data
-    data.close()
+    ID=0
+    ID+=1
+    VVent=randint(0,100)
+    DVent=randint(0,100)
+    Gite=randint(0,100)
+
+    Lon=getrandbits(0,1000000)
+    b3=(Lon>>24)
+    b2=(Lon>>16)&0xFF
+    b1=(Lon>>8)&0xFF
+    b0=(Lon>>0)&0xFF
+    
+    Lat=random.getrandbits(0,1000000)
+    
+    sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    sock.bind((server,port))
+    
+    data,addr=sock.recvfrom(1024)
+    
+#data  from client
+
+    print "Connection established (: \n"
+    print "Receive message:"
+    print "ID:",ord(data[0])
+    print "Longueur du datagram:",ord(data[1])
+    print "GV:",ord(data[2])
+    print "Safran",ord(data[3])
+    print "===========END OF PACKET===========\n"
+
+    
+    
+#résponse to client 
+    trame_res=bytearray([ID,VVent,DVent,Gite,Lon,Lat])
+    print "Envoi de la trame de réponse: ",str(trame_res).encode("hex")
+    sock.sendto(trame_res, addr)    
+    print ""
